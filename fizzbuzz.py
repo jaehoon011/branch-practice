e298e68d220c6f1ea820761ad4798882cd4e2cb1
jaehoon011 <bonkae04@gmail.com>
2026-01-15 11:17:09 +0900

    feat : Print fizzbuzz if i is Multiples of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
-for i in range(1, 16):
-    if i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0 and i % 3 != 0:
+for i in range(6, 23):
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+    elif i % 5 == 0:
         print('buzz')
+    elif i % 3 == 0:
+        print('fizz')
     else:
         print(i)
